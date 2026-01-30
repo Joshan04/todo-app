d9ecb2df5e1d281b65df955a1f4775bffb7f43f7
Joshan04 <boluwatifejoshua4@gmail.com>
2026-01-30 08:45:39 -0500

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import { Sidebar } from './components/Sidebar';
 import { MainContent } from './components/MainContent';
-import { useTaskStore } from './store/useTaskStore';
-import { useEffect } from 'react';
 
 function App() {
-  const { addTask, tasks } = useTaskStore();
 
-  // Mock Data Init
-  useEffect(() => {
-    if (Object.keys(tasks).length === 0) {
-      addTask("Review project proposal");
-      addTask("Prepare presentation slides");
-      addTask("Buy groceries for the week");
-    }
-  }, []);
+
 
   return (
     <div className="flex h-screen w-screen overflow-hidden bg-white text-gray-900 dark:bg-gray-950 dark:text-gray-100">
